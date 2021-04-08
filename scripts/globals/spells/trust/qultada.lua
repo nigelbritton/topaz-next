4d910487b6161b2ce78fd9e2d6ed5dfe35652f7a
Nigel Britton <nigel.britton@completecontrol.co.uk>
2021-04-08 21:30:50 +0100

    Update qultada.lua

--- scripts/globals/spells/trust/qultada.lua
+++ scripts/globals/spells/trust/qultada.lua
@@ -5,12 +5,35 @@ require("scripts/globals/trust")
 -----------------------------------
 local spell_object = {}
 
+local message_page_offset = 15 -- needs changing
+
 spell_object.onMagicCastingCheck = function(caster, target, spell)
     return tpz.trust.canCast(caster, spell)
 end
 
 spell_object.onSpellCast = function(caster, target, spell)
     return tpz.trust.spawn(caster, spell)
+end
+
+spell_object.onMobSpawn = function(mob)
+    tpz.trust.message(mob, message_page_offset, tpz.trust.message_offset.SPAWN)
+
+    -- TODO: Better logic than this
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.CHAOS_ROLL, ai.r.JA, ai.s.HIGHEST, tpz.jobAbility.CHAOS_ROLL)
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, tpz.effect.CORSAIRS_ROLL, ai.r.JA, ai.s.HIGHEST, tpz.jobAbility.CORSAIRS_ROLL)
+
+    -- Try and ranged attack every 60s
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.ALWAYS, 0, ai.r.RATTACK, 0, 0, 30)
+
+    mob:SetAutoAttackEnabled(false)
+end
+
+spell_object.onMobDespawn = function(mob)
+    tpz.trust.message(mob, message_page_offset, tpz.trust.message_offset.DESPAWN)
+end
+
+spell_object.onMobDeath = function(mob)
+    tpz.trust.message(mob, message_page_offset, tpz.trust.message_offset.DEATH)
 end
 
 return spell_object
