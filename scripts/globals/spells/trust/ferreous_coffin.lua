0d12af1f749e22820193560e2cd6b54cbb10482a
Nigel Britton <nigel.britton@completecontrol.co.uk>
2021-04-09 09:03:38 +0100

    Update ferreous_coffin.lua

--- scripts/globals/spells/trust/ferreous_coffin.lua
+++ scripts/globals/spells/trust/ferreous_coffin.lua
@@ -13,4 +13,23 @@ spell_object.onSpellCast = function(caster, target, spell)
     return tpz.trust.spawn(caster, spell)
 end
 
+spell_object.onMobSpawn = function(mob)
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 25, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.CURE)
+
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 75, ai.r.MA, ai.s.HIGHEST, tpz.magic.spellFamily.CURE)
+
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, tpz.effect.HASTE, ai.r.MA, ai.s.SPECIFIC, tpz.magic.spellFamily.HASTE)
+
+    mob:addSimpleGambit(ai.t.SELF, ai.c.STATUS_FLAG, tpz.effectFlag.ERASABLE, ai.r.MA, ai.s.SPECIFIC, tpz.magic.spell.ERASE)
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.STATUS_FLAG, tpz.effectFlag.ERASABLE, ai.r.MA, ai.s.SPECIFIC, tpz.magic.spell.ERASE)
+end
+
+spell_object.onMobDespawn = function(mob)
+    tpz.trust.message(mob, message_page_offset, tpz.trust.message_offset.DESPAWN)
+end
+
+spell_object.onMobDeath = function(mob)
+    tpz.trust.message(mob, message_page_offset, tpz.trust.message_offset.DEATH)
+end
+
 return spell_object
