08a698bc56e31e90e50bd897b418343f88a95e12
Code-Yeti <yetijames@gmail.com>
2021-04-12 18:05:31 +0100

    Created goblin_rush.lua file for Blue Mage spell

---
+++ scripts/globals/spells/bluemagic/goblin_rush.lua
@@ -0,0 +1,51 @@
+-----------------------------------
+-- Spell: Goblin Rush
+-- Delivers a threefold attack. Damage varies with TP
+-- Spell cost: 76 MP
+-- Monster Type: Goblin
+-- Spell Type: Physical (Blunt)
+-- Blue Magic Points: 3
+-- Stat Bonus: HP +10 DEX +2 MND -3
+-- Level: 81
+-- Casting Time: 0.5 seconds
+-- Recast Time: 25.5 seconds
+-- Skillchain Element: Fusion / Impaction
+-- Combos: None
+-----------------------------------
+require("scripts/globals/bluemagic")
+require("scripts/globals/status")
+require("scripts/globals/magic")
+-----------------------------------
+local spell_object = {}
+
+spell_object.onMagicCastingCheck = function(caster, target, spell)
+    return 0
+end
+
+spell_object.onSpellCast = function(caster, target, spell)
+    local params = {}
+    -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
+    params.tpmod = TPMOD_DAMAGE
+    params.attackType = tpz.attackType.PHYSICAL
+    params.damageType = tpz.damageType.SLASHING
+    params.scattr = SC_FUSION
+    params.numhits = 3
+    params.multiplier = 1.36
+    params.tp150 = 2.08
+    params.tp300 = 2.36
+    params.azuretp = 2.61
+    params.duppercap = 75 -- D upper >=69
+    params.str_wsc = 0.3
+    params.dex_wsc = 0.3
+    params.vit_wsc = 0.0
+    params.agi_wsc = 0.0
+    params.int_wsc = 0.0
+    params.mnd_wsc = 0.0
+    params.chr_wsc = 0.0
+    damage = BluePhysicalSpell(caster, target, spell, params)
+    damage = BlueFinalAdjustments(caster, target, spell, damage, params)
+
+    return damage
+end
+
+return spell_object
