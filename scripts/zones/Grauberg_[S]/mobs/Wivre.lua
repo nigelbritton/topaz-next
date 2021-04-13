a9cd96366c106f9ab64cfed9818be32dd4c3943e
Nigel Britton <nigel.britton@completecontrol.co.uk>
2021-04-13 11:54:46 +0100

    Create Wivre.lua

---
+++ scripts/zones/Grauberg_[S]/mobs/Wivre.lua
@@ -0,0 +1,15 @@
+-----------------------------------
+-- Area: Grauberg [S]
+--  Mob: Wivre
+--   ID: 2101
+-- Loot: Wivre maul
+-----------------------------------
+require("scripts/globals/hunts")
+require("scripts/globals/mobs")
+-----------------------------------
+local entity = {}
+
+entity.onMobDeath = function(mob, player, isKiller)
+end
+
+return entity
