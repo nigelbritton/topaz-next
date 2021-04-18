5a40b3961386495d6fe729aa6dbd149b987bc4ba
Code-Yeti <yetijames@gmail.com>
2021-04-18 21:26:09 +0100

    Add waypoint.lua for Western_Adoulin [Testing]

---
+++ scripts/zones/Western_Adoulin/npcs/Waypoint.lua
@@ -0,0 +1,32 @@
+-----------------------------------
+-- Area: Lower Jeuno (245)
+--  NPC: Waypoint
+--  SoA: Waypoint
+-- !pos 20 -34.922 0.000
+-----------------------------------
+require("scripts/globals/missions")
+require("scripts/globals/zone")
+local ID = require("scripts/zones/Western_Adoulin/IDs")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    local adoulinAccess = player:getCurrentMission(SOA) > tpz.mission.id.soa.ONWARD_TO_ADOULIN
+
+    if adoulinAccess then
+        player:startEvent(10121)
+    else
+        player:messageSpecial(ID.text.WAYPOINT_EXAMINE)
+    end
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
