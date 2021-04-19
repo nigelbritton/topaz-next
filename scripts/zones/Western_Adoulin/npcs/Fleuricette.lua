ecf3c42a6f3fb714d3a5cd86d1b31f11006f7a0f
Nigel Britton <nigel.britton@completecontrol.co.uk>
2021-04-19 21:26:59 +0100

    Create Fleuricette.lua

---
+++ scripts/zones/Western_Adoulin/npcs/Fleuricette.lua
@@ -0,0 +1,37 @@
+-----------------------------------
+-- Area: Western Adoulin
+--  NPC: Fleuricette
+-- Type: Gate NPC
+-- !pos -153 4 -16 256
+-----------------------------------
+require("scripts/globals/missions")
+require("scripts/globals/quests")
+require("scripts/globals/keyitems")
+require("scripts/globals/status")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    local bayld = player:getCurrency("bayld");
+    if SOA_Mission >= tpz.mission.id.soa.ONWARD_TO_ADOULIN and player:hasKeyItem(tpz.ki.PIONEERS_BADGE) then
+        player:startEvent(1201, bayld)
+    else
+        -- Dialogue prior to joining colonization effort
+        player:startEvent(1200)
+    end
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+    if (csid == 1201) then
+        player:delCurrency("bayld", 10)
+        player:addStatusEffect(tpz.effect.IONIS, 0, 0, 9000)
+    end
+end
+
+return entity
